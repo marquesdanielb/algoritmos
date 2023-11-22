126359c17997aa1f439e7c8736974243fd1a6407
Daniel Marques Barbosa <marquesdanielb@gmail.com>
2023-11-22 09:10:13 -0300

    Add selecao.php

--- ordenacao_por_selecao/php/selecao.php
+++ ordenacao_por_selecao/php/selecao.php
@@ -16,15 +16,12 @@ function busca_menor(array $arr): int {
 
 function ordenar_por_selecao(array $arr): array {
     $novoArr = [];
-    //var_dump(count($arr));
-    //die();
     for ($i=0; $i < count($arr); $i++) { 
         $menor = busca_menor($arr);
-        $novoArr[] = array_splice($arr, $menor, 1)[0];
+        array_push($novoArr, array_splice($arr, $menor, 1)[0]);
     }
 
     return $novoArr;
 }
 
-$selecao = ordenar_por_selecao([2, 3, 5, 6, 10]);
-//var_dump($selecao);
+$selecao = ordenar_por_selecao([5, 3, 6, 13, 10]);
